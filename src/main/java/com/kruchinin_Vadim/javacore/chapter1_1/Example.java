73527c70616e519c473e76abc6be1f48406e14fe
Vadik <gloom16@mail.ru>
2023-12-29 03:14:48 +0300

    Компиляция класса с указанием полного пути до класса включая название самого класса с расширением .java

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter1_1/Example.java
@@ -0,0 +1,17 @@
+package com.kruchinin_Vadim.javacore.chapter1_1;
+
+/*
+Это простая программа на Java.
+Присвоить исходному файлу имя "Example.java"
+ */
+public class Example {
+    // Эта программа начинается с вызова метода main
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java");
+
+    }
+}
+/*
+Компиляция - приведение исходного кода в байт-код для последующего старта программы
+Для компляции класса, в терминале необходимо прописать: javac -d bin /src/полный_путь_до_класса/Example.java
+ */
