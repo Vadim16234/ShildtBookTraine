603eadec8bfc69baaaf2444e074aaae412d8909e
Vadik <gloom16@mail.ru>
2023-12-29 03:24:03 +0300

    обьявление пцелочисленной переменной, присвоение переменной значения, операция умножения, вывод в консоль

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter1_1/Example2.java
@@ -0,0 +1,16 @@
+package com.kruchinin_Vadim.javacore.chapter1_1;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num; // обьявили переменную
+
+        num = 100; // присвоил значение переменной
+
+        System.out.println("Это переменная num: " + num);
+
+        num = num * 2;
+
+        System.out.print("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
