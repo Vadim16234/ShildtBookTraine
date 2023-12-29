315f36d5dc48b119dbe490d0c8188b606da50fb7
Vadik <gloom16@mail.ru>
2023-12-29 21:47:12 +0300

    Объявление целочисленного массива с его инициализацией без оператора new и сразу заполнили его при инициализации без указания выделения необходимого размера

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter1_1/AutoArray.java
@@ -0,0 +1,11 @@
+package com.kruchinin_Vadim.javacore.chapter1_1;
+
+// Усовершенствованная версия одномерного массива
+public class AutoArray {
+    public static void main(String[] args) {
+        // Объявили массив целых чисел, инициализировали его без оператора new и указания размера
+        int monthDay[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        System.out.println("В апреле " + monthDay[3] + " дней");
+    }
+}
