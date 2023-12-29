4e0f1dc3bf25ca1babac1de0eb381d62b3a33ec8
Vadik <gloom16@mail.ru>
2023-12-29 03:36:58 +0300

    цикл for

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter1_1/ForTest.java
@@ -0,0 +1,11 @@
+package com.kruchinin_Vadim.javacore.chapter1_1;
+
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; x++) {
+            System.out.println("Значение х: " + x);
+        }
+    }
+}
