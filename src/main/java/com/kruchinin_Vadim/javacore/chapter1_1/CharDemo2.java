472bf62f27b3de8e36b7a8b91bd1ff637e4added
Vadik <gloom16@mail.ru>
2023-12-29 15:23:43 +0300

    пример использования char c применением инкремента

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter1_1/CharDemo2.java
@@ -0,0 +1,14 @@
+package com.kruchinin_Vadim.javacore.chapter1_1;
+
+// Символьные переменные ведут себя как целочисленные значения
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 содержит " + ch1);
+
+        ch1++; // увеличить на единицу значение переменной ch1
+        System.out.println("ch1 теперь водержит " + ch1);
+    }
+}
