b009cc0d5c6bdf3676ea802698774fe72b8a9ce8
Vadik <gloom16@mail.ru>
2023-12-29 14:56:23 +0300

    пример использования переменной long

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter1_1/Light.java
@@ -0,0 +1,24 @@
+package com.kruchinin_Vadim.javacore.chapter1_1;
+
+// использование переменной long
+public class Light {
+    public static void main(String[] args) {
+        int lightSpeed;
+        long days;
+        long second;
+        long distance;
+
+        // приблизительная скорость света, миль в секунду
+        lightSpeed = 186000;
+
+        days = 1000; // указатель количесвта дней
+
+        second = days * 24 * 60 * 60; // преобразовать в секунды
+
+        distance = lightSpeed * second; // вычисление расстояния
+
+        System.out.print("За " + days);
+        System.out.print(" дней свет пройдет около ");
+        System.out.println(distance + " миль");
+    }
+}
