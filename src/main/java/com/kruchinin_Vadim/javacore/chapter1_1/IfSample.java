c376b78a82a911c523c1567272c34c3998005b50
Vadik <gloom16@mail.ru>
2023-12-29 03:30:52 +0300

    if - проверка условия

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter1_1/IfSample.java
@@ -0,0 +1,28 @@
+package com.kruchinin_Vadim.javacore.chapter1_1;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if (x < y) {
+            System.out.println("x меньше y");
+        }
+
+        x *= 2;
+        if (x == y) {
+            System.out.println("x теперь равно y");
+        }
+
+        x *= 2;
+        if (x > y) {
+            System.out.println("x теперь больше y");
+        }
+
+        if (x == y) {
+            System.out.println("Условие не выполняется, это сообщение не отобразится");
+        }
+    }
+}
