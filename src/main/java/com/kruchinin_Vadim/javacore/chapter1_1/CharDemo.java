8f5435f45976b0313e02e4eb90efa3607e5e8169
Vadik <gloom16@mail.ru>
2023-12-29 15:19:43 +0300

    пример использования char

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter1_1/CharDemo.java
@@ -0,0 +1,14 @@
+package com.kruchinin_Vadim.javacore.chapter1_1;
+
+// Демонстрация применения типа данных char
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88; // код символа Х
+        ch2 = 'Y';
+
+        System.out.print("ch1 и ch2: ");
+        System.out.print(ch1 + " " + ch2);
+    }
+}
