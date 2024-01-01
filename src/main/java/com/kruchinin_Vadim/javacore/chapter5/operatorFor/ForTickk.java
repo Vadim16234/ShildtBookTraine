662caebd526cae06dd7f3d81ed13f557e46f4b9f
Vadik <gloom16@mail.ru>
2024-01-02 01:36:03 +0300

    Объявление переменной внутри цикла for

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter5/operatorFor/ForTickk.java
@@ -0,0 +1,12 @@
+package com.kruchinin_Vadim.javacore.chapter5.operatorFor;
+
+// Обьявить переменную управления циклом в самом цикле for
+public class ForTickk {
+    public static void main(String[] args) {
+
+        // здесь переменная n объявляется в самом цикле for
+        for (int n = 10; n > 0; n--) {
+            System.out.println("такт " + n);
+        }
+    }
+}
