5db6670dbf187a973e41ca314f7c508add6a6c77
Vadik <gloom16@mail.ru>
2024-01-02 00:59:36 +0300

    Пример применения while

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter5/operatorWhile/While.java
@@ -0,0 +1,13 @@
+package com.kruchinin_Vadim.javacore.chapter5.operatorWhile;
+
+// Продемострировать применение оператора while
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+
+        while (n > 0) {
+            System.out.println("такт " + n);
+            n--;
+        }
+    }
+}
