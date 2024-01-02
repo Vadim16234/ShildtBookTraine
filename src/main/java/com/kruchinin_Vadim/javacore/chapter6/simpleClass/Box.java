c703bd33d2f8a7e5da11c5c6b28a515311625e68
Vadik <gloom16@mail.ru>
2024-01-03 01:35:16 +0300

    добавил метод, применяемы в класса BoxDemo3

--- src/main/java/com/kruchinin_Vadim/javacore/chapter6/simpleClass/Box.java
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter6/simpleClass/Box.java
@@ -4,4 +4,10 @@ public class Box {
     double width; // поля экземпляра класса
     double height;
     double depth;
+
+    // вывести объем параллелепипеда. Метод для класса BoxDemo3
+    void volume() {
+        System.out.print("Объем равен ");
+        System.out.println(width * height * depth);
+    }
 }
