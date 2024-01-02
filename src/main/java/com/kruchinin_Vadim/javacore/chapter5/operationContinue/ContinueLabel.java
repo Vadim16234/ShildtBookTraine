00d0286fce4ac8a267242e027ba8fe6d11761c24
Vadik <gloom16@mail.ru>
2024-01-02 04:48:09 +0300

    Пример применение continue c меткой

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter5/operationContinue/ContinueLabel.java
@@ -0,0 +1,17 @@
+package com.kruchinin_Vadim.javacore.chapter5.operationContinue;
+
+// Применение continue с меткой
+public class ContinueLabel {
+    public static void main(String[] args) {
+        outer: for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j ++) {
+            if (j > 1) {
+                System.out.println();
+                continue outer;
+            }
+            System.out.print(" " + (i * j));
+        }
+        }
+        System.out.println();
+    }
+}
