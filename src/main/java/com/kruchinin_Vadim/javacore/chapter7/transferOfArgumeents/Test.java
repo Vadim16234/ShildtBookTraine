e9f62e1baf6ee0e04eba0d463353cd14741e4b7a
Vadik <gloom16@mail.ru>
2024-01-04 00:55:11 +0300

    Метод, принимающий в качестве аргументов два целочисленных примитивных значения

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter7/transferOfArgumeents/Test.java
@@ -0,0 +1,9 @@
+package com.kruchinin_Vadim.javacore.chapter7.transferOfArgumeents;
+
+// Аргументы примитивных типов передаются по значению
+class Test {
+    void meth(int i, int j) {
+        i *= 2;
+        j /= 2;
+    }
+}
