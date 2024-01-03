28517195cdde166989e4a04bd608fe492b291c52
Vadik <gloom16@mail.ru>
2024-01-03 18:03:04 +0300

    Создал два обьекта класса Stack, заполнил эти объекты целочисленными значениями, а затем извлек их

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter6/stack/TestStack.java
@@ -0,0 +1,26 @@
+package com.kruchinin_Vadim.javacore.chapter6.stack;
+
+public class TestStack {
+    public static void main(String[] args) {
+        Stack myStack1 = new Stack();
+        Stack myStack2 = new Stack();
+
+        // разместить числа в стеке
+        for (int i = 0; i < 10; i++) {
+            myStack1.push(i);
+        }
+        for (int i = 10; i < 20; i++) {
+            myStack2.push(i);
+        }
+
+        // извлечь числа из стека
+        System.out.println("Содержимое стека myStack1: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(myStack1.pop());
+        }
+        System.out.println("Содержимое стека myStack2: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(myStack2.pop());
+        }
+    }
+}
