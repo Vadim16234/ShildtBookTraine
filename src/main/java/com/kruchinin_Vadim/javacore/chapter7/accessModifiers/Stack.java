c39f580807f36a5d2578d624683752e1a0561bb7
Vadik <gloom16@mail.ru>
2024-01-04 03:54:36 +0300

    Доступ к приватным данным с помощью открытых или дефолтных методов

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter7/accessModifiers/Stack.java
@@ -0,0 +1,36 @@
+package com.kruchinin_Vadim.javacore.chapter7.accessModifiers;
+
+// В этом классе определяется целочисленный стек, который может содержать 10 значений
+class Stack {
+    /*
+        Теперь переменные stck и tos являются закрытыми.
+        Это означает, что они не могут быть случайно или намеренно изменены
+        таким образом, чтобы нарушить стек.
+     */
+    private int stck[] = new int[10];
+    private int tos;
+
+    // инициализировать вершину стека
+    Stack() {
+        tos = -1;
+    }
+
+    // разместить элемент в стеке
+    void push(int item) {
+        if (tos == 9) {
+            System.out.println("Стек заполнен.");
+        } else {
+            stck[++tos] = item;
+        }
+    }
+
+    // извлечь элемент из стека
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Стек не загружен.");
+            return 0;
+        } else {
+            return stck[tos--];
+        }
+    }
+}
