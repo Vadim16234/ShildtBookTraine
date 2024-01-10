9a87bfb9720058f3e1b2d7c428ac5c13501178a5
Vadik <gloom16@mail.ru>
2024-01-10 22:44:54 +0300

    применение String

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter7/classString/StringDemo.java
@@ -0,0 +1,18 @@
+package com.kruchinin_Vadim.javacore.chapter7.classString;
+
+// Продемонстрировать применение символьных строк
+class StringDemo {
+    public static void main(String[] args) {
+        String strOb1 = "Первая строка";
+        String strOb2 = "Вторая строка";
+        String strOb3 = strOb1 + " и " + strOb2;
+
+        System.out.println(strOb1);
+        System.out.println(strOb2);
+        System.out.println(strOb3);
+    }
+}
+/*
+    В данном классе создается объект класса String
+    А так же применяется конкатенация (+) строк
+ */
