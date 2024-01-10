bd073f151eadd6b384a14628c85c2ecfa52e4136
Vadik <gloom16@mail.ru>
2024-01-11 01:49:12 +0300

    Применение ключевого слова super в конструкторе

--- src/main/java/com/kruchinin_Vadim/javacore/chapter8/accessToClassElementsAndInheritance/BoxWeight.java
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter8/accessToClassElementsAndInheritance/BoxWeight.java
@@ -9,11 +9,27 @@ package com.kruchinin_Vadim.javacore.chapter8.accessToClassElementsAndInheritanc
 class BoxWeight extends Box {
     double weight; // вес параллелепипеда
 
-    // конструктор
+    // сконструировать клон объекта
+    BoxWeight(BoxWeight ob) { // передать объект конструктору
+        super(ob);
+        weight = ob.weight;
+    }
+
+    // конструктор, применяемый при указании всех параметров
     BoxWeight(double w, double h, double d, double m) {
-        width = w;
-        height = h;
-        depth = d;
+        super(w, h, d);
+        weight = m;
+    }
+
+    // конструктор, применяемый по умолчанию
+    BoxWeight() {
+        super();
+        weight = -1;
+    }
+
+    // конструктор, применяемый при создании куба
+    BoxWeight(double len, double m) {
+        super(len);
         weight = m;
     }
 }
--- src/main/java/com/kruchinin_Vadim/javacore/chapter8/accessToClassElementsAndInheritance/DemoBoxWeight.java
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter8/accessToClassElementsAndInheritance/DemoBoxWeight.java
@@ -4,6 +4,9 @@ class DemoBoxWeight {
     public static void main(String[] args) {
         BoxWeight myBox1 = new BoxWeight(10, 20, 15, 34.3);
         BoxWeight myBox2 = new BoxWeight(2, 3, 4, 0.076);
+        BoxWeight myBox3 = new BoxWeight(); // по умолчанию
+        BoxWeight myCube = new BoxWeight(3, 2);
+        BoxWeight myClone = new BoxWeight(myBox1);
         double vol;
 
         vol = myBox1.volume();
@@ -14,5 +17,21 @@ class DemoBoxWeight {
         vol = myBox2.volume();
         System.out.println("Объем myBox2 равен " + vol);
         System.out.println("Вес myBox2 равен " + myBox2.weight);
+        System.out.println();
+
+        vol = myBox3.volume();
+        System.out.println("Объем myBox3 равен " + vol);
+        System.out.println("Вес myBox3 равен " + myBox3.weight);
+        System.out.println();
+
+        vol = myClone.volume();
+        System.out.println("Объем myClone равен " + vol);
+        System.out.println("Вес myClone равен " + myClone.weight);
+        System.out.println();
+
+        vol = myCube.volume();
+        System.out.println("Объем myCube равен " + vol);
+        System.out.println("Вес myCube равен " + myCube.weight);
+        System.out.println();
     }
 }
