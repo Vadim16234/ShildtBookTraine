d5af04ce23d7d117cb85aba054b1ab3c6f8e08b5
Vadik <gloom16@mail.ru>
2024-01-10 23:28:32 +0300

    Аргументы переменной длинны и перегрузка

---
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter7/argumentLength/VarArg3.java
@@ -0,0 +1,34 @@
+package com.kruchinin_Vadim.javacore.chapter7.argumentLength;
+
+// Аргументы переменной длинны и перегрузка
+class VarArg3 {
+    static void vaTest(int ... v) {
+        System.out.print("vaTest(int ...): " + "Количество аргументов: " + v.length + " Содержимое: ");
+        for(int x : v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    static void vaTest(boolean ... v) {
+        System.out.print("vaTest(boolean ...): " + "Количество аргументов: " + v.length + " Содержимое: ");
+        for(boolean x : v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    static void vaTest(String msg, int ... v) {
+        System.out.print("vaTest(String msg, int ...): " + "Количество аргументов: " + msg + v.length + " Содержимое: ");
+        for(int x : v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        vaTest(1, 2, 3);
+        vaTest("Проверка: ", 10, 20);
+        vaTest(true, false, false);
+    }
+}
