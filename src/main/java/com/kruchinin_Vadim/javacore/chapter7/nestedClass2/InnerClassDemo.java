00a882b420d939989e44ab5e03b0b8b00e254aa7
Vadik <gloom16@mail.ru>
2024-01-10 22:39:53 +0300

    вложенный класс в цикле

--- src/main/java/com/kruchinin_Vadim/javacore/chapter7/nestedClass2/InnerClassDemo.java
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter7/nestedClass2/InnerClassDemo.java
@@ -1,6 +1,6 @@
 package com.kruchinin_Vadim.javacore.chapter7.nestedClass2;
 
-public class InnerClassDemo {
+class InnerClassDemo {
     public static void main(String[] args) {
         Outer outer = new Outer();
         outer.test();
