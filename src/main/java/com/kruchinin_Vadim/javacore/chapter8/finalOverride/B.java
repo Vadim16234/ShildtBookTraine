b52802e84dd68ce095ebf7a02663529b427a350b
Vadik <gloom16@mail.ru>
2024-01-12 01:19:42 +0300

    Применение final к методу и невозможность его переопределения в подклассе

--- src/main/java/com/kruchinin_Vadim/javacore/chapter8/finalOverride/B.java
+++ src/main/java/com/kruchinin_Vadim/javacore/chapter8/finalOverride/B.java
@@ -1,7 +1,7 @@
 package com.kruchinin_Vadim.javacore.chapter8.finalOverride;
 
 class B extends A {
-    void meth() { // ОШИБКА! Этот метод не может быть переопределен.
-        System.out.println("Недопустимо!!!");
-    }
+//    void meth() { // ОШИБКА! Этот метод не может быть переопределен.
+//        System.out.println("Недопустимо!!!");
+//    }
 }
